6bf9349474cecf3ff0a9821ab53fc744bf582cf8
HotSauceVL <vova030494@gmail.com>
2023-04-02 16:59:47 +1000

    refactor: может так

--- statistics/client/src/main/java/ru/practicum/explorewithme/statclient/WebClientConfig.java
+++ statistics/client/src/main/java/ru/practicum/explorewithme/statclient/WebClientConfig.java
@@ -1,7 +1,6 @@
 package ru.practicum.explorewithme.statclient;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
@@ -15,10 +14,12 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfig {
+
+    private static final String serverUrl = "https://localhost:9090";
     private static final int TIMEOUT = 5000;
 
     @Bean
-    public WebClient webClientWithTimeout(@ Value ("${stats-server.url}") String serverUrl) {
+    public WebClient webClientWithTimeout() {
         final HttpClient httpClient = HttpClient.create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
                 .responseTimeout(Duration.ofMillis(TIMEOUT))
